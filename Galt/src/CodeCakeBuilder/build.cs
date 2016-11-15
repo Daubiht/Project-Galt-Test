ce4006deb8aa9b2f53384faccc921b15cf5c4875
daubiht <daubihtmania@gmail.com>
2016-11-15 15:56:47 +0100

    aezaezaeaz

--- Galt/src/CodeCakeBuilder/build.cs
+++ Galt/src/CodeCakeBuilder/build.cs
@@ -3,6 +3,7 @@ using Cake.Common.Tools.DotNetCore;
 using Cake.Core;
 using Cake.Core.IO;
 using Cake.Common.Tools.OpenCover;
+using Cake.Common.Tools.NUnit;
 
 namespace CodeCake
 {
@@ -52,22 +53,24 @@ namespace CodeCake
 
             Task( "Unit-Tests" )
                 .IsDependentOn( "Build" )
-                .Does( () => {
-                    DirectoryPathCollection projectPaths = Cake.GetDirectories( "./*.Tests" );
-                    foreach( DirectoryPath path in projectPaths )
+                .Does( () =>
+                {
+                    Cake.OpenCover( tool =>
                     {
-                        //Cake.DotNetCoreRun( path.FullPath );
-
-                        Cake.OpenCover( tool =>
+                        tool.NUnit3( "./**/*.Tests/**/net46/*/**/*.Tests.exe", new NUnit3Settings
                         {
-                            tool.DotNetCoreRun( path.FullPath );
-                        },
-                         new FilePath( "./result.xml" ),
-                         new OpenCoverSettings()
-                            .WithFilter( "+[Test]*" )
-                            .WithFilter( "-[Test.Tests]*" )
-                        );
-                    }
+                            ToolPath = @"C:\Users\Plop\git\Project-Galt-Test\Galt\packages\NUnit.ConsoleRunner\3.5.0\tools\nunit3-console.exe"
+                        } );
+                    },
+                     new FilePath( "./result.xml" ),
+                     new OpenCoverSettings
+                     {
+                         ToolPath = "C:/Users/Plop/git/Project-Galt-Test/Galt/packages/OpenCover/4.6.519/tools/OpenCover.Console.exe",
+                         Register = "User"
+                     }
+                        .WithFilter( "+[Test]*" )
+                        .WithFilter( "-[Test.Tests]*" )
+                    );
                 } );
 
             // The Default task for this script can be set here.
